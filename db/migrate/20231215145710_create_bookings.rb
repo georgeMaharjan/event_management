fc5a4cb93488497d5c7b06b2a5cb874404ed9cff
George Maharjan <georgeiemaharjan12@gmail.com>
2023-12-15 22:53:07 +0545

    feat: create booking model and migration

---
+++ db/migrate/20231215145710_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.string :name
+      t.string :email
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
