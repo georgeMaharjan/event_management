58050218bc12b2fbe477f45eb51d8023a6d7aed5
George Maharjan <georgeiemaharjan12@gmail.com>
2023-12-16 09:17:38 +0545

    feat: update logic to create booking - add logic to display user bookings - create partials for booking - update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/bookings', to: 'bookings#index'
+  get '/user-bookings', to: 'bookings#user_bookings', as: 'user_bookings'
   devise_for :users
   resources :events do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
