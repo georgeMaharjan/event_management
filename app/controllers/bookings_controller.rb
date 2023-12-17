4a7e63f0de38ba48f2e1db410275d639cb820db4
George Maharjan <georgeiemaharjan12@gmail.com>
2023-12-17 17:15:34 +0545

    feat:  update logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,10 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
+  before_action :set_booking, only: %i[ destroy ]
 
   def index
     @bookings = Booking.all
-    authorize! :index, @bookings
   end
 
   def user_bookings
@@ -13,11 +14,30 @@ class BookingsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     @user = User.find(params[:user_id])
-    @booking = Booking.new(event_id: @event.id, user_id: @user.id)
-    if @booking.save
-      redirect_to root_path, notice: "You have booked #{@event.title}"
+    existing_booking = Booking.find_by(event_id: @event.id, user_id: @user.id)
+
+    if existing_booking
+      redirect_to root_path, alert: "You have already booked this event."
     else
-      render :new
+      @booking = Booking.new(event: @event, user: @user)
+
+      if @booking.save
+        redirect_to user_bookings_path, notice: "You have booked #{@event.title}"
+      end
     end
+  end
+
+  def destroy
+    @booking.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to user_bookings_path, notice: "Booking was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
