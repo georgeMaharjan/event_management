2e6cbcb5bbf3b14ce8570bfdfb803a164df21b10
George Maharjan <georgeiemaharjan12@gmail.com>
2023-12-17 13:23:36 +0545

    feat: update logic to redirect to login when not authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: "You are not authorized to access this page."
   end
+
+  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_unauthenticated_access
+
+  private
+
+  def handle_unauthenticated_access
+    # Redirect to the login page or do something else
+    redirect_to new_user_session_path, alert: 'You need to sign in to access this page.'
+  end
 end
